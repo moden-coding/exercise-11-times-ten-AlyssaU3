a45aa0e2f5b80fc0774fda94d5ea7e416c5d9576
AlyssaU3 <alys4321c@gmail.com>
2025-09-03 16:46:40 -0400

    exersise 11 done

--- src/times_ten.py
+++ src/times_ten.py
@@ -1,8 +1,20 @@
 # Write your solution here
 def times_ten(start_index: int, end_index: int):
-    pass
+    
+   
+    dictionary={}
+    
+    my_list=[]
+    for g in range(start_index, end_index + 1):
+        my_list.append(g)
+        dictionary[g]= g*10
+       
+    for i in range(start_index, end_index):
+           
+        return dictionary
 
 
 if __name__ == "__main__":
     d = times_ten(3, 6)
     print(d)
+    
